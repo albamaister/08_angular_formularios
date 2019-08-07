957386e3a81469ad74f531bdf27718e39e224f8b
Andres Alba <bryanalba92@gmail.com>
2019-08-07 18:01:59 -0500

    Validaciones y NgClass

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -10,7 +10,7 @@ export class DataComponent  {
   forma: FormGroup;
   constructor() {
     this.forma = new FormGroup({
-      'nombre': new FormControl('', Validators.required),
+      'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
       'apellido': new FormControl('', Validators.required),
       'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
     });
