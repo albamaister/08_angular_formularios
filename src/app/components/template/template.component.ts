f55cfdd4f9aad9a5baf0d425b281fc33f31b8f6f
Andres Alba <bryanalba92@gmail.com>
2019-08-07 12:49:05 -0500

    Uso de selects, radio y

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -12,9 +12,23 @@ import { NgForm } from '@angular/forms';
 })
 export class TemplateComponent implements OnInit {
   usuario = {
-    nombre: 'Bryan',
-    apellido: 'Alba',
-    correo: '' };
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais: '',
+    sexo: "Hombre",
+    acepta: false };
+
+    paises = [{
+      codigo: "CRI",
+      nombre: "Costa Rica"
+    },
+    {
+      codigo: "ESP",
+      nombre: "España"
+    }]
+
+    sexos: string[] = ["Hombre","Mujer","Sin definir"];
 
   constructor() { }
 
