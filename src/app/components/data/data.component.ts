624fbdb18ec0ff36c3c2e77dcb1178bea5bd66c6
Andres Alba <bryanalba92@gmail.com>
2019-08-07 19:53:06 -0500

    Agrupaciones de los objetos-formGroupName

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -8,16 +8,29 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class DataComponent  {
   forma: FormGroup;
+  usuario: Object = {
+    nombrecompleto: {
+      nombre: 'Nombre',
+      apellido: 'Apellido'
+    },
+    correo: 'correo@correo.com'
+  };
+
   constructor() {
+    console.log(this.usuario);
     this.forma = new FormGroup({
-      'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
-      'apellido': new FormControl('', Validators.required),
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
+        'apellido': new FormControl('', Validators.required),
+      }),
       'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+
     });
    }
 
    guardarCambios() {
-     console.log(this.forma.value);
+    console.log(this.forma.value);
+    console.log(this.forma);
    }
 
 
