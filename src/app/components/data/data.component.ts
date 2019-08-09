f04808eae74a949a3c3e17ee5c71130c22053976
Andres Alba <bryanalba92@gmail.com>
2019-08-09 10:59:03 -0500

    Validaciones

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-data',
@@ -27,10 +28,44 @@ export class DataComponent  {
       'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
       'pasatiempos' : new FormArray([
         new FormControl('Correr', Validators.required)
-      ])
+      ]),
+      'username' : new FormControl('', Validators.required, this.existeUsuario),
+      'password1' : new FormControl('', Validators.required),
+      'password2' : new FormControl()
 
     });
     // this.forma.setValue(this.usuario);
+    this.forma.controls['password2'].setValidators([
+      Validators.required,
+      this.noIgual.bind(this.forma)
+    ]);
+   }
+
+   noIgual( control: FormControl ): {[s: string]: boolean} {
+     let forma: any = this;
+     if (control.value !== forma.controls['password1'].value) {
+       return {
+         noiguales: true
+       }
+     }
+     return null;
+   }
+
+   // creacion de proceso asincrono
+
+   existeUsuario(control: FormControl): Promise<any> | Observable<any>{
+     let promesa = new Promise(
+       (resolve, reject) => {
+         setTimeout(() => {
+            if (control.value === 'strider') {
+              resolve({existe: true});
+            } else {
+              resolve(null);
+            }
+         }, 3000);
+       }
+     );
+     return promesa;
    }
 
    agregarPasatiempo() {
@@ -42,13 +77,13 @@ export class DataComponent  {
    guardarCambios() {
     console.log(this.forma.value);
     console.log(this.forma);
-    this.forma.reset({
-      nombrecompleto: {
-        nombre: '',
-        apellido: ''
-      },
-      correo: ''
-    });
+    // this.forma.reset({
+    //   nombrecompleto: {
+    //     nombre: '',
+    //     apellido: ''
+    //   },
+    //   correo: ''
+    // });
    }
 
 
